a5424ed5f06bf53404d0d4ec9c54d209a1a280c6
buntybably <nikhilratnaparkhi12@gmail.com>
2022-06-24 12:48:04 +0530

    USMA-3:Push spring Boot project into git hub repository

---
+++ src/main/java/com/bikkadIT/demo/UserMangementAppApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIT.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMangementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMangementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikkadIT/demo/UserMangementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIT.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMangementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
